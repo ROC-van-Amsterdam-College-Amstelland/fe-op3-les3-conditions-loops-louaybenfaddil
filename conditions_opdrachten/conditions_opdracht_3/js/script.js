3ee0ad48d9f1e019f278467205b07a280bca98ea
louay benfaddil <louay1@live.nl>
2019-12-02 15:37:29 +0100

    .

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,14 +1,15 @@
 //javascript code
-functioncheck(){
+function check(){
     var saldo = document.getElementById("saldo").value;
      
     if(saldo==500){
-            document.getElementById("tekst").innerHTML ="uw salaris is gestort!"
-    }elseif(saldo>500){
-                document.getElementById("tekst").innerHTML ="uw salaris met uw bonus is gestort!"
+            document.getElementById("tekst").innerHTML ="uw salaris is gestort!";
+    }else if(saldo>500){
+                document.getElementById("tekst").innerHTML ="uw salaris met uw bonus is gestort!";
      
     }else{
             document.getElementById("tekst").innerHTML ="Er is geen geld gestort!";
     }
      
-    }
+
+}

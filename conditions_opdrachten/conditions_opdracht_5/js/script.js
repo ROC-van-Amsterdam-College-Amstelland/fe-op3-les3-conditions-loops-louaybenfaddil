c96f4bf5afdfbc78c6e581fb89261dfc2c2baf9d
louay benfaddil <louay1@live.nl>
2019-12-03 14:31:23 +0100

    af

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+function optellen() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    document.getElementById("antwoord").innerHTML=getal1+getal2
+ }
+  
+ function aftrekken() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    document.getElementById("antwoord").innerHTML=getal1-getal2
+  
+ } 
--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+function keer() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    document.getElementById("antwoord").innerHTML=getal1/getal2
+ }
+ 
+ function delen() {
+    var getal1 = parseInt(document.getElementById("getal1").value);
+    var getal2 = parseInt(document.getElementById("getal2").value);
+    document.getElementById("antwoord").innerHTML=getal1*getal2
+ 
+ } 
